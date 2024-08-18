575cab1ca24ba23b38db08ae520e66a0a57ceefd
FajarRiansyah <fajary781@gmail.com>
2024-08-18 15:46:55 +0700

    add all service route

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,20 @@ Volt::route('/dedicated-team', 'pages.collaborationtype.dedicated-team')->name('
 Volt::route('/on-demand', 'pages.collaborationtype.on-demand')->name('on-demand');
 Volt::route('/portfolio', 'pages.portfolio.portfolio')->name('portfolio');
 Volt::route('/portfolio/{id}', 'pages.portfolio.detail-portfolio')->name('detail-portfolio');
-Volt::route('/service', 'pages.services.service')->name('service');
-Volt::route('/service/{id}', 'pages.services.service-detail')->name('service-detail');
+// Volt::route('/service/{id}', 'pages.services.service-detail')->name('service-detail');
+
+Route::get('/service', fn() => view('service.service'))->name('service');
+Route::prefix('service')->group(function () {
+    Route::get('/web-dev', fn() => view('service.website-development'))->name('s-web-dev');
+    Route::get('/mobile-dev', fn() => view('service.mobile-app-development'))->name('s-mobile-dev');
+    Route::get('/product-dev', fn() => view('service.product-development'))->name('s-product-dev');
+    Route::get('/custom-software-dev', fn() => view('service.custom-software-development'))->name('s-custom-dev');
+    Route::get('/ui-ux-design', fn() => view('service.ui-ux-design'))->name('s-ui-ux');
+    Route::get('/devops-solution', fn() => view('service.devops-solution'))->name('s-devops');
+    Route::get('/system-testing', fn() => view('service.system-testing'))->name('s-system-testing');
+    Route::get('/big-data', fn() => view('service.big-data-service'))->name('s-big-data');
+    Route::get('/digital-marketing', fn() => view('service.digital-marketing'))->name('s-digital-marketing');
+});
+
 
 require __DIR__ . '/auth.php';
