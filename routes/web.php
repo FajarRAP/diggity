7484b06bcacacc7c42d61c2cffc53e1e69ea5df9
FajarRiansyah <fajary781@gmail.com>
2024-08-27 19:36:04 +0700

    fix error when calling php file

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ Volt::route('/contactus', 'pages.contactus.contact-us')->name('contact-us');
 Volt::route('/portfolio', 'pages.portfolio.portfolio')->name('portfolio');
 Volt::route('/portfolio/{id}', 'pages.portfolio.detail-portfolio')->name('detail-portfolio');
 
-require __DIR__ . 'collaboration-type.php';
-require __DIR__ . 'service.php';
-require __DIR__ . 'product.php';
+require __DIR__ . '/collaboration-type.php';
+require __DIR__ . '/service.php';
+require __DIR__ . '/product.php';
 require __DIR__ . '/auth.php';
